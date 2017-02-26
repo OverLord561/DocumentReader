29fc9e03c441b7d2bdbcc2eceadeb987cca75450
overlord561 <yurapuk452@gmail.com>
2017-02-26 23:36:38 +0200

    first demo

---
+++ DocumentReader/Figure.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DocumentReader
+{
+
+
+    public abstract class Figure
+    {
+        public DateTime startOfExecuting = DateTime.Now;
+        public string message;
+        public abstract double GetSquare();
+
+        public void  WriteToFile(string message)
+        {
+            Program.ShowProgressDel del = new Program.ShowProgressDel(Program.ShowProgress);
+            
+            string logfilePath = String.Empty;
+
+            string nameOfFile = startOfExecuting.ToString();
+            nameOfFile = nameOfFile.Replace(':', '_'); // to solve incorrect name of file. Windows exception.
+            logfilePath = Path.Combine(Directory.GetCurrentDirectory(), nameOfFile + ".txt");
+
+            Thread myThread = new Thread(()=>
+           // Task myThread = Task.Factory.StartNew(()=>
+            {
+                
+               
+                Thread.Sleep(2222);
+                if (File.Exists(logfilePath))
+                {
+                    File.AppendAllText(logfilePath, message + Environment.NewLine);
+                }
+                else
+                {
+                    using (FileStream fs = File.Create(logfilePath))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes(message + Environment.NewLine);
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+                
+
+            });
+             myThread.Start();
+
+            //while (myThread.IsAlive)
+            //{
+            //    del.Invoke();
+            //};
+
+        }
+
+        //print into console, write to file
+        public  void DisplayProcess(string name)
+        {
+            message = string.Format("{0}: {1}",name, GetSquare());
+            Console.WriteLine(message);
+            WriteToFile(message);
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        public double a;
+        public double b;
+        public double c;
+
+
+        public Triangle(double[] param, string name)
+        {
+            try
+            {
+                this.a = param[0];
+                this.b = param[1];
+                this.c = param[2];
+
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                //message = string.Format("Tringle: {0}", GetSquare());
+                //Console.WriteLine(message);
+                //WriteToFile(message);
+                DisplayProcess(name);
+
+            }
+
+            // in case when we do not have all 3 sizes
+            catch
+            {
+                return;
+            }
+
+
+        }
+
+        public override double GetSquare()
+        {
+            double p = (a + b + c) / 2;
+            double square = Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 5);
+
+            if (square.ToString() == "NaN")
+            {
+                // in case when we incorrect sizes
+                throw new Exception();
+            }
+
+            return square;
+        }
+    }
+
+    public class Square : Figure
+    {
+        public double a;
+        public Square(double[] param, string name)
+        {
+            this.a = param[0];
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            DisplayProcess(name);
+        }
+
+        public override double GetSquare()
+        {
+            return Math.Pow(a, 2);
+        }
+
+    }
+
+    public class Circle : Figure
+    {
+        public double radius;
+        public Circle(double[] param, string name)
+        {
+            this.radius = param[0];
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            DisplayProcess(name);
+        }
+
+        public override double GetSquare()
+        {
+            return Math.Round(Math.PI * Math.Pow(radius, 2), 5);
+        }
+    }
+
+    public class Ellipse : Figure
+    {
+        public double a;
+        public double b;
+
+        public Ellipse(double[] param, string name)
+        {
+            try
+            {
+                this.a = param[0];
+                this.b = param[1];
+                Console.ForegroundColor = ConsoleColor.White;
+
+                DisplayProcess(name);
+            }
+            // in case when we do not have all 2 sizes
+            catch
+            {
+                return;
+            }
+            
+        }
+
+        public override double GetSquare()
+        {
+            return Math.Round(Math.PI * a * b, 5);
+        }
+    }
+
+    public class Rectangle : Figure
+    {
+        public double a;
+        public double b;
+
+        public Rectangle(double[] param, string name)
+        {
+            try
+            {
+                this.a = param[0];
+                this.b = param[1];
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                DisplayProcess(name);
+            }
+            catch
+            {
+                return;
+            }
+            
+        }
+        public override double GetSquare()
+        {
+            return a * b;
+        }
+    }
+
+    
+}
---
+++ DocumentReader/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace DocumentReader
+{
+    class Program
+    {
+        public static string pathToFile { get; set; }
+        public delegate void ShowProgressDel();
+        public static void ShowProgress()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write(".");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
+            string nameSpaceName = "DocumentReader"; 
+
+            var pathToFile = Path.Combine(Directory.GetCurrentDirectory(), "Test doc.txt");
+            string[] data = File.ReadAllLines(pathToFile); //source file with test data
+
+            foreach (string line in data)
+            {
+                Thread.Sleep(300);
+
+                string[] separatedData = line.Split(' '); // split data by white spaces
+                var separatedDataLIST = separatedData.ToList(); // convert to list cause i want to remove extra symbols from source file 
+                separatedDataLIST.Remove("");
+                separatedDataLIST.Remove(",");
+
+                int countOfElements = separatedDataLIST.Count;
+
+
+                // get clear data from previous list
+                string[] clearData = new string[countOfElements]; 
+                int j = 0;
+                foreach (string data1 in separatedDataLIST)
+                {
+                    clearData[j] = data1;
+                    j++;
+                }
+                           
+
+                string className = clearData[0]; // returns Square, so class is Square, Circle, Rectangle, Triangle, Ellipse 
+                double[] parameters = new double[clearData.Length - 1]; // new array for parameters (sizes of figures)
+                for (int i = 1; i < clearData.Length; i++)
+                {
+
+                    try
+                    {
+                        parameters[i - 1] = Convert.ToDouble(clearData[i].TrimEnd(',')); // in case when multi parameters, separated by ',' : 5,6, where 5,6 is size
+                    }
+                    catch
+                    {
+                        parameters[i - 1] = Convert.ToDouble(clearData[i]);// in case when single parameters
+                    }
+
+
+                }
+
+                try
+                {
+                    //if name of Class is incorrect we sent this exception to catch block, not to UnhandledExceptionTrapper
+                    var newInstance = Activator.CreateInstance(Type.GetType(nameSpaceName + "." + className),
+                                     new Object[] { parameters, clearData[0] });
+
+                    // if instance constructor is not available it returns null
+                    Figure figure = (Figure)newInstance;
+                    if (figure.message == null)
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error in input parameters: {0}", line);
+                }
+
+
+            }
+            Task.WaitAll();
+           // ShowProgress();
+
+
+
+        }
+
+        // in case when we have incorrect data like "5/" or "5|"
+        static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception exception = (Exception)e.ExceptionObject;
+            Console.WriteLine(exception.Message.ToString());
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            
+            Environment.Exit(1);
+        }
+
+
+
+    }
+}
